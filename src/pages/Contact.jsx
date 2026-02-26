a969c324851cf6c1cbd72a50bdf83a48d0025e6c
pauladim <pauladim92@gmail.com>
2026-02-26 13:08:32 +0100

    Updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -51,10 +51,10 @@ const templateParams = {
 
 
       const response = await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        import.meta.env.EMAILJS_SERVICE_ID,
+        import.meta.env.EMAILJS_TEMPLATE_ID,
         templateParams,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        import.meta.env.EMAILJS_PUBLIC_KEY
       );
 
       if (response.status === 200) {
